32375ef8662e12ff9ba9ccd87301610400a65103
Josh Sandquist <joshdsand@gmail.com>
2023-03-28 17:23:19 -0700

    added writeFile code as well as created a title question

--- Develop/index.js
+++ Develop/index.js
@@ -4,14 +4,32 @@ const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's title?",
+      },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description for your project.'
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('Your README file has been generated!')
+    );
+  }
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+      const markdown = generateMarkdown(data);
+      writeToFile('README.md', markdown);
+    });
+  }
 
 // Function call to initialize app
 init();
