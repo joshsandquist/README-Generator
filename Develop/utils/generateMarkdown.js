f0c27af89ede3f9b666ba5e25ecfebb486183978
Josh Sandquist <joshdsand@gmail.com>
2023-03-29 17:13:50 -0700

    added license functions to markdown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,53 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// This function will take in the user input for license and generate the correct badge
+// shields.io was used to pull in badges
+// If no license is selected, an empty string will be returned
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)';
+  } else if (license === 'Apache') {
+    return '![Apache License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+  } else if (license === 'GPL') {
+    return '![GPL License](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// This function takes in the same perameters as the previous funtion, but now will generate a link to the license's page
+  function renderLicenseLink(license) {
+    if (license === 'MIT') {
+      return '[MIT License](https://opensource.org/licenses/MIT)';
+    } else if (license === 'Apache') {
+      return '[Apache License](https://opensource.org/licenses/Apache-2.0)';
+    } else if (license === 'GPL') {
+      return '[GPL License](https://www.gnu.org/licenses/gpl-3.0)';
+    } else {
+      return '';
+    }
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// This function will generate the markdown section for a licesnce, and include the selected license within the section.
+// No section will be generated if a license is not selected.
+function renderLicenseSection(license) {
+  if (license === 'MIT') {
+    return `## License
+
+This project is licensed under the ${renderLicenseLink(license)}.
+    `;
+  } else if (license === 'Apache 2.0') {
+    return `## License
+
+This project is licensed under the ${renderLicenseLink(license)}.
+    `;
+  } else if (license === 'GPLv3') {
+    return `## License
+
+This project is licensed under the ${renderLicenseLink(license)}.
+    `;
+  } else {
+    return '';
+  }
+}
 
 function generateMarkdown(data) {
   return `# ${data.title}
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # README-Generator
 A node application to generate a professional README
+
 ## Installation
 Requires npm and the intstalling the inquirer version 8.2.4 package
+
+## Description
+This project was designed to generate a high quality README file by taking in user input using nodejs. Running the file will prompt the user
+to answer multiple questions that will then generate a new README file taking in their inputs.
+
+## Usage
+To run this application, first install all required dependencies. After installation, run the command 'node index' to initialize the file.
+
+## Contributing
+N/A
+
+## Testing
+
+N/A
+
+## Screenshots
+
+
+## Github Repository
